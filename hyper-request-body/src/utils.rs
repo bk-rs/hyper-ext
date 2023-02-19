4bb1fff4452c8874ab322962832aad72db025f3f
vkill <vkill.net@gmail.com>
2023-02-19 16:46:47 +0800

    Update hyper-request-body

--- hyper-request-body/src/utils.rs
+++ hyper-request-body/src/utils.rs
@@ -1,18 +1,8 @@
 use bytes::{Bytes, BytesMut};
 use futures_util::{Stream, StreamExt as _};
-use hyper::{Body as HyperBody, Error as HyperError};
+pub use hyper_body_to_bytes::{hyper_body_to_bytes, hyper_body_to_bytes_with_max_length};
 
 use crate::Error;
-
-// Copy from warp_request_body::utils::hyper_body_to_bytes
-pub async fn hyper_body_to_bytes(mut hyper_body: HyperBody) -> Result<Bytes, HyperError> {
-    let mut bytes_mut = BytesMut::new();
-    while let Some(bytes) = hyper_body.next().await {
-        let bytes = bytes?;
-        bytes_mut.extend_from_slice(&bytes[..]);
-    }
-    Ok(bytes_mut.freeze())
-}
 
 pub async fn bytes_stream_to_bytes(
     mut stream: impl Stream<Item = Result<Bytes, Error>> + Unpin,
@@ -28,15 +18,6 @@ pub async fn bytes_stream_to_bytes(
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[tokio::test]
-    async fn test_hyper_body_to_bytes() {
-        let hyper_body = HyperBody::from("foo");
-        assert_eq!(
-            hyper_body_to_bytes(hyper_body).await.unwrap(),
-            Bytes::copy_from_slice(b"foo")
-        );
-    }
 
     #[tokio::test]
     async fn test_bytes_stream_to_bytes() {
